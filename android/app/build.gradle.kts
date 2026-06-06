bb305be227b49eeb604fc584b0043f130df83178
huzaifa137 <huzaifabukenya227@gmail.com>
2026-06-06 15:47:25 +0300

    qwerty

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,18 +15,19 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
+    namespace = "com.techsate.senteclick"   // ✅ Added missing namespace (matches applicationId)
     compileSdk = 36
     ndkVersion = "28.2.13676358"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17   // was VERSION_11
-        targetCompatibility = JavaVersion.VERSION_17  // was VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17  // was JVM_11
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
         }
     }
 
