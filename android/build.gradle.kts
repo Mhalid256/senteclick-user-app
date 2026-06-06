ee165205ca31627858df22355bad3010eb641111
huzaifa137 <huzaifabukenya227@gmail.com>
2026-06-06 15:59:25 +0300

    qwerty

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,30 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+    // Force Java 17 for all Java compilation tasks (including plugins)
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
+    // Force JVM target 17 for all Kotlin compilation tasks
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
     }
 }
 
